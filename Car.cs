42dbdc5e88afc602bd4f1cecf2ef7bcb7c4cdced
shalbuzz <rovshannashvor@gmail.com>
2024-11-29 21:52:54 +0400

    add

---
+++ Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_enum
+{
+     public class Car
+    {
+
+        private static int _counter ;
+        public int Id { get; set; }
+
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public Type Type { get; set; }
+        public Car(string brand, string model,Type type)
+        {
+            Id = ++_counter;
+            Brand = brand;
+            Model = model;
+            Type = type;
+        }
+        public override string ToString()
+        {
+            return $"{Id}-{Brand}-{Model}-{Type}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task_enum
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("mercedes", "e200",Type.Sport);
+            Car car1 = new Car("bmw", "f10",Type.Sedan);
+            Console.WriteLine(car);
+            Console.WriteLine(car1);
+        }
+    }
+}
